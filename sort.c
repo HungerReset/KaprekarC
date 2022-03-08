07b761917771d3938fdda95023f3e61242921d07
HungerReset <99591824+HungerReset@users.noreply.github.com>
2022-03-08 10:26:35 +0100

    Removed unnecessary header files

--- sort.c
+++ sort.c
@@ -1,18 +1,15 @@
-#include <stdio.h>
-#include <stdbool.h>
-
 int * sort (int array[], int len) {
 	
-	bool sorted = false;
+	bool sorted = 0;
 	int t = 0;
 	while (!sorted) {
-		sorted = true;
+		sorted = 1;
 		for (int i = 0; i < len; ++i) {
 			if (array[i] > array[i+1] && i != len - 1) {
 				int t = array[i];
 				array[i] = array[i+1];
 				array[i+1] = t;
-				sorted = false;
+				sorted = 0;
 			}
 		}
 	}
